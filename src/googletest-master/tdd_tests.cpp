dbf5084e9b506663a808596e8afc04d6f58c9546
Tomas <tomas.lapsansky.97@gmail.com>
2017-04-10 14:13:35 +0200

    Zakladna matematika + testy 	modified:   src/googletest-master/CMakeFiles/tdd_test.dir/CXX.includecache 	modified:   src/googletest-master/CMakeFiles/tdd_test.dir/mnt/hgfs/FIT/2sem/IVS/proj2/src/math_library.cpp.o 	modified:   src/googletest-master/CMakeFiles/tdd_test.dir/tdd_tests.cpp.o 	modified:   src/googletest-master/tdd_test 	modified:   src/googletest-master/tdd_tests.cpp 	modified:   src/math_library.cpp 	modified:   src/math_library.h

--- src/googletest-master/tdd_tests.cpp
+++ src/googletest-master/tdd_tests.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "../math_library.h"
 
-class Math_test : public ::testing::Test
+class Math_test_plus : public ::testing::Test
 {
 public:
 
@@ -9,7 +9,7 @@ public:
 	
 };
 
-TEST_F(Math_test, Plus_decimal_numbers)
+TEST_F(Math_test_plus, Decimal_numbers)
 {
 	EXPECT_EQ(plus(5, 5, &error), 10);
 	EXPECT_FALSE(error);
@@ -27,17 +27,17 @@ TEST_F(Math_test, Plus_decimal_numbers)
 	EXPECT_FALSE(error);
 }
 
-TEST_F(Math_test, Plus_limits)
+TEST_F(Math_test_plus, Limits)
 {	
-	EXPECT_EQ(plus(DBL_MAX, 5, &error), 0);
+	EXPECT_EQ(plus(DBL_MAX, 5, &error), DBL_MAX);
 	EXPECT_TRUE(error);
 	
 	error = false;
-	EXPECT_EQ(plus(DBL_MAX, 2.5, &error), 0);
+	EXPECT_EQ(plus(DBL_MAX, 2.5, &error), DBL_MAX);
 	EXPECT_TRUE(error);
 }
 
-TEST_F(Math_test, Plus_float_numbers)
+TEST_F(Math_test_plus, Float_numbers)
 {
 	EXPECT_EQ(plus(15.2501, 14.2501, &error), 29.5002);
 	EXPECT_FALSE(error);
@@ -45,6 +45,302 @@ TEST_F(Math_test, Plus_float_numbers)
 	EXPECT_EQ(plus(15.2501, -14.2501, &error), 1);
 	EXPECT_FALSE(error);
 	
-	EXPECT_EQ(plus(-25.12, 55.12, &error), 30);
+	EXPECT_EQ(plus(-30.1, 0.1, &error), -30);
 	EXPECT_FALSE(error);
+}
+
+class Math_test_minus : public ::testing::Test
+{
+public:
+
+	bool error = false;
+	
+};
+
+TEST_F(Math_test_minus, Decimal_numbers)
+{
+	EXPECT_EQ(minus(10, 5, &error), 5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(15, 16, &error), -1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(14, -5, &error), 19);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(-13, -5, &error), -8);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(0, 5, &error), -5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(10, 0, &error), 10);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_minus, Limits)
+{	
+	EXPECT_EQ(minus(-DBL_MAX, 5, &error), -DBL_MAX);
+	EXPECT_TRUE(error);
+	
+	error = false;
+	EXPECT_EQ(minus(-DBL_MAX, 2.5, &error), -DBL_MAX);
+	EXPECT_TRUE(error);
+}
+
+TEST_F(Math_test_minus, Float_numbers)
+{
+	EXPECT_EQ(minus(15.2501, 14.2501, &error), 1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(5.002, 0.002, &error), 5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(minus(25.12, -55.12, &error), 80.24);
+	EXPECT_FALSE(error);
+}
+
+class Math_test_multiple : public ::testing::Test
+{
+public:
+
+	bool error = false;
+	
+};
+
+TEST_F(Math_test_multiple, Decimal_numbers)
+{
+	EXPECT_EQ(multi(10, 2, &error), 20);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(-5, 3, &error), -15);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(5, -30, &error), -150);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(-4, -5, &error), 20);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(0, 5, &error), 0);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(5, 0, &error), 0);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(0, 0, &error), 0);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_multiple, Float_numbers)
+{
+	EXPECT_EQ(multi(2.2, 3.3, &error), 7.26);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(-2.2, 3.3, &error), -7.26);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(multi(2.2, 0, &error), 0);
+	EXPECT_FALSE(error);
+}
+
+class Math_test_division : public ::testing::Test
+{
+public:
+
+	bool error = false;
+	
+};
+
+TEST_F(Math_test_division, Decimal_numbers)
+{
+	EXPECT_EQ(divis(10, 2, &error), 5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(10, -2, &error), -5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(5, 1, &error), 5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(5, -1, &error), -5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(-10, 5, &error), -2);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(-10, -5, &error), 2);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(0, 5, &error), 0);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_division, Zero_division)
+{
+	EXPECT_EQ(divis(10, 0, &error), 0);
+	EXPECT_TRUE(error);
+
+	error = false;
+	EXPECT_EQ(divis(-10, 0, &error), 0);
+	EXPECT_TRUE(error);
+
+	error = false;
+	EXPECT_EQ(divis(10.5, 0, &error), 0);
+	EXPECT_TRUE(error);
+}
+
+TEST_F(Math_test_division, Float_numbers)
+{
+	EXPECT_EQ(divis(12.5, 5, &error), 2.5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(10, 2.5, &error), 4);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(divis(-10, 2.5, &error), -4);
+	EXPECT_FALSE(error);
+}
+
+class Math_test_factorial : public ::testing::Test
+{
+public:
+
+	bool error = false;
+	
+};
+
+TEST_F(Math_test_factorial, Basic)
+{
+	EXPECT_EQ(fact(0, &error), 1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(fact(1, &error), 1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(fact(2, &error), 2);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(fact(5, &error), 120);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_factorial, Bad_numbers)
+{
+	EXPECT_EQ(fact(-1, &error), 0);
+	EXPECT_TRUE(error);
+	
+	error = false;
+	EXPECT_EQ(fact(2.5, &error), 0);
+	EXPECT_TRUE(error);
+	
+	error = false;
+	EXPECT_EQ(fact(-10.1, &error), 0);
+	EXPECT_TRUE(error);
+}
+
+class Math_test_power : public ::testing::Test
+{
+public:
+
+	bool error = false;
+	
+};
+
+TEST_F(Math_test_power, Decimal_numbers)
+{
+	EXPECT_EQ(power(5, 0, &error), 1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(10, 1, &error), 10);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(2, 6, &error), 64);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(-2, 2, &error), 4);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(-2, 3, &error), -8);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_power, Float_numbers)
+{
+	EXPECT_EQ(power(2.2, 0, &error), 1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(10.5, 1, &error), 10.5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(3.5, 3, &error), 42.875);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(-3.5, 3, &error), -42.875);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(power(-3.5, 4, &error), 150.0625);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_power, Limits)
+{
+	EXPECT_EQ(power(DBL_MAX, 2, &error), INFINITY);
+	EXPECT_TRUE(error);
+}
+
+TEST_F(Math_test_power, Bad_exponent)
+{
+	EXPECT_EQ(power(50, -1, &error), 0);
+	EXPECT_TRUE(error);
+	
+	error = false;
+	EXPECT_EQ(power(50, 2.5, &error), 0);
+	EXPECT_TRUE(error);
+	
+	error = false;
+	EXPECT_EQ(power(50, -2.5, &error), 0);
+	EXPECT_TRUE(error);
+}
+
+class Math_test_root : public ::testing::Test
+{
+public:
+
+	bool error = false;
+	
+};
+
+TEST_F(Math_test_root, Decimal_numbers)
+{
+	EXPECT_EQ(root(0, &error), 0);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(root(1, &error), 1);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(root(4, &error), 2);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(root(9, &error), 3);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_root, Float_numbers)
+{
+	EXPECT_EQ(root(6.25, &error), 2.5);
+	EXPECT_FALSE(error);
+	
+	EXPECT_EQ(root(198.81, &error), 14.1);
+	EXPECT_FALSE(error);
+}
+
+TEST_F(Math_test_root, Bad_numbers)
+{
+	EXPECT_EQ(root(-5, &error), 0);
+	EXPECT_TRUE(error);
+	
+	error = false;
+	EXPECT_EQ(root(-2.2, &error), 0);
+	EXPECT_TRUE(error);
 }
--- src/math_library.h
+++ src/math_library.h
@@ -1,15 +1,16 @@
 #include <stdbool.h>
 #include <limits.h>
+#include <math.h>
 
-	double plus(double x, double y, bool *error);
-	double minus(double x, double y, bool *error);
-	double multi(double x, double y, bool *error);
-	double divis(double x, double y, bool *error);
-	
-	double fact(double x, bool *error);
-	
-	double power(double x, double n, bool *error);
-	
-	double root(double x, bool *error);
-	
-	double log(double x, bool *error);
+double plus(double x, double y, bool *error);
+double minus(double x, double y, bool *error);
+double multi(double x, double y, bool *error);
+double divis(double x, double y, bool *error);
+
+double fact(double x, bool *error);
+
+double power(double x, double n, bool *error);
+
+double root(double x, bool *error);
+
+double log(double x, bool *error); //bonus
