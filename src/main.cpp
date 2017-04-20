817dfb220836bde43d0813940d547202a6ed38a7
draon7 <ondra.de@seznam.cz>
2017-04-20 16:50:00 +0200

    Update main.cpp
    Pridana hlavicka k souboru main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+/******************************************************************************
+ * Název projektu: IVS-2.-projekt
+ * Soubor: main.cpp
+ * Datum:  7.4.2017
+ * Poslední změna: 20.4.2017
+ * Autor:  David Dejmal xdejma00
+ *
+ * Popis: soubor main.cpp, obsahuje funkci main
+ *        
+ ******************************************************************************/
+
+/**
+ * @file main.cpp
+ * 
+ * @brief obsahuje funkci main
+ */
 #include "mainwindow.h"
 #include <QApplication>
 
